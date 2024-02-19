bb96de7a8639a6c72885453789e8af8752df21a2
Mingjie.Zhu22 <mingjie.zhu22@student.xjtlu.edu.cn>
2024-02-19 09:46:40 +0800

    complete proj1gold

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 public class TestArrayDequeGold {
     StudentArrayDeque<Integer> input = new StudentArrayDeque<>();
     ArrayDequeSolution<Integer> expected = new ArrayDequeSolution<>();
+    StringBuilder log = new StringBuilder();
 
     @Test
     public void Test1() {
@@ -16,31 +17,35 @@ public class TestArrayDequeGold {
                 if (one2two() == 1) {
                     input.addFirst(i);
                     expected.addFirst(i);
+                    log.append("addFirst(").append(i).append(")\n");
                 }
                 if (one2two() == 2) {
                     input.addLast(i);
                     expected.addLast(i);
+                    log.append("addLast(").append(i).append(")\n");
                 }
             } else {
                 if (one2four() == 1) {
                     input_ = input.removeFirst();
                     expected_ = expected.removeFirst();
-                    assertEquals("Index is " + i + ", expected(" + expected.size()
-                            + "), input(" + expected.size() + ")", expected_, input_);
+                    log.append("removeFirst()\n");
+                    assertEquals(log.toString(), expected_, input_);
                 }
                 if (one2four() == 2) {
                     input_ = input.removeLast();
                     expected_ = expected.removeLast();
-                    assertEquals("Index is " + i + ", \nexpected: size(" + expected.size()
-                            + ") \ninput: size(" + expected.size() + ")", expected_, input_);
+                    log.append("removeLast()\n");
+                    assertEquals(log.toString(), expected_, input_);
                 }
                 if (one2four() == 3) {
                     input.addLast(i);
                     expected.addLast(i);
+                    log.append("addLast(").append(i).append(")\n");
                 }
                 if (one2four() == 4) {
                     input.addFirst(i);
                     expected.addFirst(i);
+                    log.append("addFirst(").append(i).append(")\n");
                 }
             }
         }
